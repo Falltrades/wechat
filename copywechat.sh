4584e1dce14ca7eaa0eaa764d17255aff543209d
Vincent <thai.nguyen>
2023-05-27 21:39:42 +0700

    update

--- copywechat.sh
+++ copywechat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-dir=/home/$USER/Images/Captures\ d’écran/
+USER=??
+dir=/home/$USER/Pictures/Screenshots
+#dir=/home/$USER/Images/Captures\ d’écran/
 screenshot=$(ls "$dir" | tail -1)
 docker_id=$(docker ps |grep wechat |awk '{print $1}')
 docker cp "$dir"/"$screenshot" $docker_id:/test
